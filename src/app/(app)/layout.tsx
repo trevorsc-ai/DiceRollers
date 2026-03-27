552e9fbf94f1425d75adc77a35a3e38513ddbdee
Travis Scavone <263262441+trevorsc-ai@users.noreply.github.com>
2026-03-27 07:51:13 -0400

    fix: disable Mickey's puzzle modal on login

--- src/app/(app)/layout.tsx
+++ src/app/(app)/layout.tsx
@@ -19,7 +19,7 @@ export default async function AppLayout({
         {children}
       </main>
       <BottomNav />
-      <MickeysPuzzleModal />
+      {/* <MickeysPuzzleModal /> */}
     </div>
   );
 }
